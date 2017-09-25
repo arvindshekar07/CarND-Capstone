9c7f94140bd6b306e8491e8dff540330a8984d31
Mahendra P Singh <kunwarmahen@gmail.com>
2017-09-24 22:07:03 -0400

    Add files via upload
    Fixed traffic light changes and some bug fixes

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -22,66 +22,66 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 200  # Number of waypoints we will publish. You can change this number
+LOOKAHEAD_WPS = 50 # Number of waypoints we will publish. You can change this number
 SPEED = 10
-
+END_INDEX = 10
 
 class WaypointUpdater(object):
     def __init__(self):
-        rospy.init_node('waypoint_updater')
+		rospy.init_node('waypoint_updater')
 
-        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
-        rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
-        rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
+		rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
+		rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
+		rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
-
-
-        self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
+		self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
-        self.pose = None
-        self.waypoints = None
-        self.tf_light_index = None
+		self.pose = None
+		self.waypoints = None
+		self.tf_light_index = None
 
-        self.loop()
+		self.loop()
 
     def loop(self):
-        rate = rospy.Rate(50)  # 50Hz
+		rate = rospy.Rate(50) # 50Hz
+        
+		while not rospy.is_shutdown():
+			rate.sleep()
+			lane = Lane()
 
-        while not rospy.is_shutdown():
-            rate.sleep()
-            lane = Lane()
+			if self.waypoints is not None and self.pose is not None:
+				closet_waypoint_index = self.get_closest_waypoint_index(self.waypoints.waypoints, self.pose)
+				end_index = None
+				start_index = None
+				if self.tf_light_index != -1 and self.tf_light_index >= closet_waypoint_index and self.tf_light_index <=closet_waypoint_index+LOOKAHEAD_WPS:
+					end_index = self.tf_light_index - closet_waypoint_index
+					start_index = end_index - END_INDEX
+					if start_index < 0:
+						start_index = 0
+				
+				lane.waypoints = self.waypoints.waypoints[closet_waypoint_index:closet_waypoint_index+LOOKAHEAD_WPS]
 
-            if self.waypoints is not None and self.pose is not None:
-                # lane.header = self.waypoints.header
-                closet_waypoint_index = self.get_closest_waypoint_index(self.waypoints.waypoints, self.pose)
-                # print(self.tf_light_index)
-                new_tf_light_index = None
-                if self.tf_light_index >= closet_waypoint_index and self.tf_light_index <= closet_waypoint_index + LOOKAHEAD_WPS:
-                    new_tf_light_index = self.tf_light_index - closet_waypoint_index
-                lane.waypoints = self.waypoints.waypoints[
-                                 closet_waypoint_index:closet_waypoint_index + LOOKAHEAD_WPS]
-
-                for waypoint in lane.waypoints:
-                    waypoint.twist.twist.linear.x = SPEED
-
-                if new_tf_light_index is not None:
-                    lane.waypoints[new_tf_light_index].twist.twist.linear.x = 0.0
-
-                self.final_waypoints_pub.publish(lane)
-
+				for waypoint in lane.waypoints:
+					waypoint.twist.twist.linear.x = SPEED
+				
+				if end_index is not None:
+					for waypoint in lane.waypoints[start_index:end_index]:
+						waypoint.twist.twist.linear.x = 0.0
+				
+				self.final_waypoints_pub.publish(lane)
+			
+			
+		
     def pose_cb(self, msg):
         # TODO: Implement
-        self.pose = msg.pose
-
-        # pass
+		self.pose = msg.pose
+	
 
     def waypoints_cb(self, waypoints):
         # TODO: Implement
         self.waypoints = waypoints
-
-        # pass
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
@@ -99,27 +99,28 @@ class WaypointUpdater(object):
 
     def distance(self, waypoints, wp1, wp2):
         dist = 0
-        dl = lambda a, b: math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2 + (a.z - b.z) ** 2)
-        for i in range(wp1, wp2 + 1):
+        dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
+        for i in range(wp1, wp2+1):
             dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
             wp1 = i
         return dist
 
     def get_closest_waypoint_index(self, waypoints, pose):
-        dist = float('inf')
-        index = 0
-        dl = lambda a, b: math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2 + (a.z - b.z) ** 2)
-        for waypoint in waypoints:
-            temp_dist = dl(waypoint.pose.pose.position, pose.position)
-            if temp_dist < dist:
-                dist = temp_dist
-                index = index + 1
+		dist = float('inf')
+		index = 0
+		selected_index = None
+		dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
+		for waypoint in waypoints:
+			temp_dist = dl(waypoint.pose.pose.position, pose.position)
+			if temp_dist < dist:
+				dist = temp_dist
+				selected_index = index
+			index = index + 1
 
-        if waypoints[index].pose.pose.position.x < self.pose.position.x:
-            index = index + 1
-
-        return index
-
+		if waypoints[selected_index].pose.pose.position.x < self.pose.position.x:
+			selected_index = selected_index + 1
+	
+		return selected_index
 
 if __name__ == '__main__':
     try:
